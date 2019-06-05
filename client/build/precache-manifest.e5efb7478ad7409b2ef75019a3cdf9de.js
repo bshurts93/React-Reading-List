b5299d17b522c17eda9d5ccf19d7cad942840d41
Ben Shurts <bshurts93@gmail.com>
2019-06-04 21:30:10 -0500

    Ran build for final static files

---
+++ client/build/precache-manifest.e5efb7478ad7409b2ef75019a3cdf9de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2cb42375a5bbe1c1ff33",
+    "url": "/static/js/main.007bec71.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "2cb42375a5bbe1c1ff33",
+    "url": "/static/css/main.a587ad5c.chunk.css"
+  },
+  {
+    "revision": "361866dd2f83942fd891cfe20b2ad24c",
+    "url": "/index.html"
+  }
+];
